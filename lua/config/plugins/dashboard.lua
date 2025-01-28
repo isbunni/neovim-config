b528526d16960f56dbb0c48e385a465a2cc05057
isbunni <bunnibriscoe@gmail.com>
2025-01-27 17:51:29 -0800

    Dashboard Fixes

--- lua/config/plugins/dashboard.lua
+++ lua/config/plugins/dashboard.lua
@@ -38,23 +38,14 @@ return{
           }, --your header
           center = {
             {
-              icon = ' ',
               icon_hl = 'Title',
-              desc = 'Find File           ',
+              desc = 'Find File     ',
               desc_hl = 'String',
-              key = 'b',
-              keymap = 'SPC f f',
+              keymap = 'SPC f f     ',
+              key = 'f',
               key_hl = 'Number',
               key_format = ' %s', -- remove default surrounding `[]`
-              action = 'lua print(2)'
-            },
-            {
-              icon = ' ',
-              desc = 'Find Dotfiles',
-              key = 'f',
-              keymap = 'SPC f d',
-              key_format = ' %s', -- remove default surrounding `[]`
-              action = 'lua print(3)'
+              action = 'Telescope find_files'
             },
           },
           footer = {}  --your footer
